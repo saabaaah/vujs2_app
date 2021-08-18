c9ed521c2caad336d5b2e691aacef6f13f125957
Hamou Maamar Sabah <saabaaah@gmail.com>
2021-08-18 16:45:53 +0100

    + adding methods

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ new Vue({
     el: '#main-content',
     data: {
         post: " VueJS APP test",
-        editor: "sabah-HM"
+        editor: "sabah-HM",
+        note: "Excellent! "
+    },
+    methods: {
+        editorNotes: function(){
+            // getting today's date :
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+
+            today = dd + '/' + mm + '/' + yyyy;
+            return `This is ${this.note} ! Today is ${today}}`;
+        }
     }
 })
